3bee45eab278a1a9e300026b6989b27a51ca0e39
jayounghoyos <juanandresyounghoyos@gmail.com>
2024-03-25 14:54:06 -0500

    Readme con preguntas

--- README.md
+++ README.md
@@ -8,3 +8,15 @@
 
 <h3>Esquema del proyecto</h3>
 <img src="./Scheme.jpg">
+
+<h3>Preguntas asociadas al problema</h3>
+
+RESPONDA A LAS SIGUIENTES PREGUNTAS Y ADJUNTE A LA PROPUESTA
+DE SOLUCIÓN
+
+1. ¿Cuáles son las clases necesarias para resolver este problema?
+2. Para cada clase, presente los métodos más importantes.
+3. ¿En qué parte de la solución se deberían aplicar PILAS?
+4. ¿En qué parte de la solución se deberían aplicar COLAS?
+5. ¿Es más eficiente utilizar PILAS y COLAS con LISTAS ENLAZADAS?, ¿Por
+qué?
--- main.py
+++ main.py
@@ -10,11 +10,67 @@ class colors: #Creo la clase colors
     MAGENTA = '\033[95m'
     CYAN = '\033[96m'
 
-Class Node:
+class Node:
+    def __init__(self, valor):
+        self.valor = valor
+        self.siguiente = None
+
+class Pila: #Se crea la clase pila
+    def __init__(self): # Se crea el constructor con una lista vacía
+        self.items = []
+
+    def apilar(self, elemento): #Método de apilamiento
+        self.items.append(elemento)
+
+    def desapilar(self): #Eliminación del último elemento por ser una pila LIFO
+        if not self.esta_vacia():
+            return self.items.pop()
+        return None
+
+    def esta_vacia(self):# Verificación ¿vació o no?
+        return len(self.items) == 0
+
+    def imprimir(self):#Imprime la pila
+        print(colors.YELLOW + "Contenido de la pila:" + colors.RESET)
+        for elemento in self.items:
+            print(elemento)
+
+class Cola:
+    def __init__(self):
+        self.primero = None
+        self.ultimo = None
+
+    def encolar(self, elemento):
+        nuevo_nodo = Node(elemento)
+        if self.ultimo:
+            self.ultimo.siguiente = nuevo_nodo
+        self.ultimo = nuevo_nodo
+        if not self.primero:
+            Self_primero = nuevo_nodo
+
+    def desencolar(self):
+        if self.primero:
+            valor_retirado = self.primero.valor
+            self.primero = self.primero.siguiente
+            if not self.primero:
+                self.ultimo = None
+            return valor_retirado
+        else:
+            return None
+
+    def esta_vacia(self):
+        return self.primero is None
+
+    def imprimir(self):
+        print(colors.BLUE + "La cola tiene: " + colors.RESET)
+        nodo_actual = self.primero
+        while nodo_actual:
+            print(nodo_actual)
+            nodo_actual = nodo_actual.siguiente
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print('PyCharm')
+    print('PyC')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
